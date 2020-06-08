776d5ffa49a8e2b2d4dfebac716a4baf51ca2b71
pythonnewb7734 <aaron.bruchhauser@students.mq.edu.au>
2020-06-08 22:39:42 +1000

    Ex2

---
+++ Ex2.py
@@ -0,0 +1,6 @@
+# A comment, this is so you can read tour program later
+# Anything after the # is ignored by python
+
+print("I could have code like this.") #This comment is ignored.
+
+#print("This wont run")
