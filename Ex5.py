32ca43c948e2a744797e716234f16c0f8d5e1e78
pythonnewb7734 <aaron.bruchhauser@students.mq.edu.au>
2020-06-09 12:25:45 +1000

    First set of Ex

---
+++ Ex5.py
@@ -0,0 +1,18 @@
+my_name= 'Aaron (AB) Bruchhauser'
+my_age = 22 #Jealous?
+my_height = 183 #cm
+my_weight = 90 #kg
+my_eyes = 'Green'
+my_teeth = 'Red'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} cm tall")
+print(f"He's {my_height} kilos heavy")
+print("Actually that's not to heavy")
+print(f"He's got {my_eyes} eys and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} after the toothbrush")
+
+#this line is tricky, try to get it right
+total = my_age +my_height +my_weight
+print(f"If I add {my_age}, and {my_weight}, I get {total}")
